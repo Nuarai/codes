7990f5b050b2cefd680dfae63654bb407741b211
Nuarai <151392541+Nuarai@users.noreply.github.com>
2025-08-17 19:43:40 +0300

    Add files via upload

--- 6.py
+++ 6.py
@@ -4,16 +4,18 @@
 n, k, m = map(int, input().split())
 all_pairs = [input().split() for _ in range(m)]
 
-lst = [0] * n
+array = [0] * n
 
-for _ in range(m):
-    st, end = map(int, all_pairs[_])
+for i in range(m):
+    start, end = map(int, all_pairs[i])
     flag = 1
-    for j in range(st, end):
-        if lst[j] + 1 > k:
+    
+    for j in range(start, end):
+        if array[j] + 1 > k:
             flag = 0
             break
-        elif lst[j] + 1 <= k:
-            lst[j] += 1
+        elif array[j] + 1 <= k:
+            array[j] += 1
+            
     print(flag)
 
